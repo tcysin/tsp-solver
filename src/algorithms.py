dce38e5c7399ea11633d8752df18208be1e03e46
Aleksei Tcysin <aleksei.tcysin@gmail.com>
2019-06-01 11:12:07 +0200

    Init algorithms and tests for them

--- src/algorithms.py
+++ src/algorithms.py
@@ -19,35 +19,39 @@ FUNCTIONS
 Lower bound + tight lower bound on shortest tour
 """
 
-from graph import Graph
+from src.graph import Graph
 
 
 # TODO:
 
 def shortest_tour(graph, algorithm='genetic'):
     """Returns a list of nodes representing (approximate) shortest tour.
-    
-    Available algorithms: 'greedy', 'genetic', 'brute_force'.
 
-    Arguments
+    Available algorithms: 'brute_force', 'genetic', 'greedy'.
+
+    Params
     ---
-        graph (Graph) -- isntance of a Graph
+        graph (Graph) -- isntance of Graph class
     """
 
-    if isinstance(graph, Graph):
+    if not isinstance(graph, Graph):
+        raise TypeError('Provided object is not a Graph.')
 
-        if algorithm == 'genetic':
-            # return _genetic(self)
-            raise NotImplementedError()
-        elif algorithm == 'greedy':
-            return greedy(graph)
-        else:
-            raise ValueError('Passed algorithm is invalid.')
-
+    if algorithm == 'brute_force':
+        # return _brute_force
+        raise NotImplementedError()
+    if algorithm == 'genetic':
+        # return _genetic(self)
+        raise NotImplementedError()
+    elif algorithm == 'greedy':
+        return _greedy(graph)
     else:
-        raise TypeError('Provided graph is not an instance of a Graph.') 
+        raise ValueError('Passed algorithm is invalid.')
+
 
 # --- shortest tour finding algorithms ---
+
+
 def genetic(graph):
     # TODO: implement genetic algorithm for shortest tour finding
 
@@ -60,14 +64,16 @@ def genetic(graph):
     # updating the population
 
     # stopping condition
+    pass
 
-def greedy(graph):
-    """Finds shortest tour using greedy approach."""
+
+def _greedy(graph):
+    """Finds shortest tour using greedy approach.
+
+    O(n^2).
+    """
 
     tour = []
-    # TODO: think about re-design of the whole thing with using
-        # just tour list and going on until the list is full?
-    # TODO: document why set
     available_nodes = set(graph.nodes())
 
     # pick random starting node in a graph, add it to tour path
@@ -75,12 +81,12 @@ def greedy(graph):
     tour.append(prev_node)
 
     while available_nodes:
-        # TODO: maybe get prev node from end of current tour list?
+
         # pick next node out of available nodes based on distance
         next_node = min(
-            [candidate for candidate in available_nodes],
-            key=lambda candidate: graph.distance(prev_node, candidate)
-            )
+            (candidate for candidate in available_nodes),
+            key=lambda x: graph.distance(prev_node, x)
+        )
 
         tour.append(next_node)
         available_nodes.remove(next_node)
