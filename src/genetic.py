06e3ee867250662219a9d34f22f854f42300de4a
Aleksei Tcysin <aleksei.tcysin@gmail.com>
2019-06-01 13:09:43 +0200

    Init genetic

--- src/genetic.py
+++ src/genetic.py
@@ -28,13 +28,15 @@ MUTATION_PROBA = 0.1
 # ----- Helper functions -----
 def mutate(chromosome, func):
     """Applies provided mutation func to chromosome."""
-    
+
     return func(chromosome)
+
 
 def crossover(main_parent, secondary_parent, func):
     """Applies provided crossover func to main_parent and secondary_parent."""
-    
+
     return func(main_parent, secondary_parent)
+
 
 def generate_swath(sequence_length):
     """Generates random slice for sequence of given length."""
@@ -44,20 +46,23 @@ def generate_swath(sequence_length):
 
     return slice(first_cut, last_cut)
 
+
 def get_fitness(chromosome, graph):
     """Returns fitness of a chromosome. Tour length in our case."""
-    
+
     return round(abs(graph.get_tour_length(chromosome) - graph.upper_bound()))
 
-def get_average_fitness(population):
-    """Gets average fitness of population."""
-    
+
+def mean_fitness(population):
+    """Returns average fitness of population."""
+
     avg = mean(item[0] for item in population)
     return round(avg, 2)
 
+
 def fill_missing_genes(prefilled_slice, source, target):
     """Uses source's remaining alleles to fill missing genes in target."""
-    
+
     source_length = len(source)
     start_point = prefilled_slice.stop
 
@@ -78,8 +83,8 @@ def fill_missing_genes(prefilled_slice, source, target):
 
         if value not in redundant_values:
             # wrap around
-            idx_target = ( (target_offset + start_point) 
-                % source_length )
+            idx_target = ((target_offset + start_point)
+                          % source_length)
 
             # fill in target's gene with parent's allele
             target[idx_target] = value
@@ -89,15 +94,16 @@ def fill_missing_genes(prefilled_slice, source, target):
 
     return
 
+
 def select_parent(population):
     """Selects two chromosomes from population with Roulette Wheel selection.
-    
+
     Potvin, J.-Y. (1996). Genetic algorithms for the TSP.
     """
 
     # sum up fitnesses over whole population
     total = sum(item[0] for item in population)
-    
+
     # generate random number in range [0, total - 1)
     num = randint(0, total - 1)
 
@@ -105,7 +111,7 @@ def select_parent(population):
     # chosen number
     running_sum = 0.
     for item in population:
-        
+
         # TODO: the problem is somewhere here
         running_sum += item[0]
 
@@ -116,9 +122,10 @@ def select_parent(population):
     print('hitting overflow error')
     return population[0][1]
 
+
 def select_two_parents(population):
     """Selects two different parent chromosomes from population."""
-    
+
     parent1 = select_parent(population)
     parent2 = select_parent(population)
 
@@ -127,16 +134,18 @@ def select_two_parents(population):
     while parent1 == parent2:
         parent2 = select_parent(population)
     """
-    
+
     return parent1, parent2
+
 
 def is_population_saturated(population):
     """Checks wheter all values in population are same."""
     return len(set(fitness for (fitness, _) in population)) <= 1
 
+
 def update_population(item, population):
     """Updates population with new item if item is good enough.
-    
+
     If provided items fitness is better than that of worst item in
     population, remove worst item and add provided item.
     """
@@ -154,10 +163,15 @@ def update_population(item, population):
 #   Adaptive Crossover Operators for GA to Resolve TSP.
 def generate_population(graph):
     """Generates initial population of (fitness, possible solution) items.
-    
+
     Uses heuristic to approximate shortest tour and then
-    repeatedly mutates it to construct other members. 
-    """ 
+    repeatedly mutates it to construct other members.
+
+    Returns
+    ---
+        list: contains tuples (fitness score, tour)
+    """
+
     population = []
 
     # use heuristics to construct first individual
@@ -187,7 +201,7 @@ def generate_population(graph):
 # ----- Crossover Operators -----
 def ox1(main_parent, secondary_parent):
     """Returns a result of OX1 order crossover between parents.
-    
+
     Randomly chooses a subtour of main_parent and copies it
     into a child. Then uses secondary_parent to fill in 
     missing genes, preserving relative order of parent's genes.
@@ -216,12 +230,11 @@ def ox1(main_parent, secondary_parent):
     # check if the swath contains only 1 member
     while swath.stop - swath.start <= 1:
         swath = generate_swath(length)
-    
 
     # make sure the swath does not cover the whole parent
     while swath.start == 0 and swath.stop == length:
         swath = generate_swath(length)
-    
+
     # and copy it from main parent into a child at the same indices
     child[swath] = main_parent[swath]
 
@@ -231,9 +244,11 @@ def ox1(main_parent, secondary_parent):
     return child
 
 # ----- Mutation Operators -----
+
+
 def sim(chromosome):
     """Apply simple inversion mutator to a chromosome.
-    
+
     Modifies in-place by reversing random sub-sequence. 
     """
 
@@ -243,7 +258,6 @@ def sim(chromosome):
     # check if the swath contains only 1 member
     while swath.stop - swath.start <= 1:
         swath = generate_swath(len(chromosome))
-    
 
     # reverse that portion
     chromosome[swath] = reversed(chromosome[swath])
@@ -257,52 +271,51 @@ def genetic(graph):
     population = generate_population(graph)
 
     # get initial fitness of the population
-    prev_mean_fitness = get_average_fitness(population)
+    prev_mean_fitness = mean_fitness(population)
 
     no_improvement_counter = 0
 
-    # stopping condition 1: MAX_ITERATION limit reached
-    for iteration in range(MAX_ITERATIONS):
-        
-        # --- make sure population is not overly saturated ---
+    # stopping condition - MAX_ITERATIONS limit reached
+    for _ in range(MAX_ITERATIONS):
+
+        # make sure population is not overly saturated
         if is_population_saturated(population):
             print('Population oversaturated. Returning best solution.')
             break
-        
-        # --- select parents from the population ---
+
+        # select parents from the population
         parent1, parent2 = select_two_parents(population)
 
-        # --- apply OX1 crossover w. high probability proba_crossover ---
-        if random() <= CROSSOVER_PROBA:      
+        # apply OX1 crossover w. high probability proba_crossover
+        if random() <= CROSSOVER_PROBA:
             child1 = crossover(parent1, parent2, ox1)
         if random() <= CROSSOVER_PROBA:
-            child2 = crossover(parent2, parent1, ox1)        
+            child2 = crossover(parent2, parent1, ox1)
 
-        # --- apply SIM mutation w. low proba_mutation ---
+        # apply SIM mutation w. low proba_mutation
         if random() <= MUTATION_PROBA:
             mutate(child1, sim)
         if random() <= MUTATION_PROBA:
             mutate(child2, sim)
 
-        # --- replace ancestors with children in a population ---
+        # replace ancestors with children in a population
         child1_item = get_fitness(child1, graph), child1
         update_population(child1_item, population)
 
-        # do same for child2
         child2_item = get_fitness(child2, graph), child2
         update_population(child2_item, population)
 
-        # --- check for no improvement ---
-        current_average_fitness = get_average_fitness(population)
-        is_fitness_improved = current_average_fitness > prev_mean_fitness
-    
+        # check for no improvement
+        current_mean_fitness = mean_fitness(population)
+        is_fitness_improved = current_mean_fitness > prev_mean_fitness
+
         # reassign prev_average_fitness to be current value
-        prev_mean_fitness = current_average_fitness
-        
+        prev_mean_fitness = current_mean_fitness
+
         if not is_fitness_improved:
             no_improvement_counter += 1
-        # in case of improvement, reset the counter
         else:
+            # in case of improvement, reset the counter
             no_improvement_counter = 0
 
         if no_improvement_counter >= ITERATIONS_WITHOUT_IMPROVEMENT:
